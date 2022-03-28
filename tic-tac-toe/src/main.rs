57654637a624f3f0f70d758b03e6e9aa464d0420
Mahmoud Fathi <m.fathi1991@gmail.com>
2022-03-28 19:54:01 +0200

    Add the functionality to determine the winner.

--- tic-tac-toe/src/main.rs
+++ tic-tac-toe/src/main.rs
@@ -1,3 +1,5 @@
+extern crate core;
+
 mod player;
 
 use crate::player::Player;
@@ -12,12 +14,18 @@ fn main() {
         let input = read_number();
         status.insert(input, turn.symbol());
         draw(&status);
-
-        turn = new_turn(turn);
+        if let Some(p) = get_winner(&status) {
+            println!("{:?} won!", p);
+            break;
+        }
+        turn = turn.opposite();
     }
 }
 
 fn draw(status: &HashMap<i32, String>) {
+    // Clear the terminal
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+
     println!(
         " {} | {} | {} ",
         unwrap_or_empty(status.get(&1)),
@@ -68,10 +76,33 @@ fn read_number() -> i32 {
     }
 }
 
-fn new_turn(turn: Player) -> Player {
-    if turn == Player::Player1 {
-        Player::Player2
-    } else {
-        Player::Player1
+fn get_winner(status: &HashMap<i32, String>) -> Option<Player> {
+    let winner_moves = vec![
+        vec![1, 2, 3],
+        vec![4, 5, 6],
+        vec![7, 8, 9],
+        vec![1, 4, 7],
+        vec![2, 5, 8],
+        vec![3, 6, 9],
+        vec![1, 5, 9],
+        vec![3, 5, 7],
+    ];
+
+    let mut player_map: HashMap<Player, Vec<i32>> = HashMap::new();
+    for (board_number, player_symbol) in status {
+        if !player_symbol.eq(" ") {
+            let player = Player::from_symbol(player_symbol);
+            let moves = player_map.entry(player).or_insert(Vec::new());
+            moves.push(*board_number);
+        }
     }
+
+    for (player, moves) in player_map {
+        for winner_move in &winner_moves {
+            if winner_move.iter().all(|wm| moves.contains(wm)) {
+                return Option::Some(player);
+            }
+        }
+    }
+    Option::None
 }
--- tic-tac-toe/src/player.rs
+++ tic-tac-toe/src/player.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, PartialEq)]
+use crate::Player::{Player1, Player2};
+
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub enum Player {
     Player1,
     Player2,
@@ -9,6 +11,21 @@ impl Player {
         match *self {
             Player::Player1 => "X".to_string(),
             Player::Player2 => "O".to_string(),
+        }
+    }
+
+    pub fn opposite(&self) -> Player {
+        match *self {
+            Player::Player1 => Player::Player2,
+            Player::Player2 => Player::Player1,
+        }
+    }
+
+    pub fn from_symbol(symbol: &str) -> Player {
+        match symbol {
+            "X" => Player1,
+            "O" => Player2,
+            _ => panic!("Unknown symbol"),
         }
     }
 }
